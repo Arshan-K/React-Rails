92910598334a635d4e5c0dd022765ce66378fb92
Arshan <arshankhan9326863763@gmail.com>
2024-11-27 13:08:53 +0530

    cors

--- music_band_search/app/controllers/api/v1/bands_controller.rb
+++ music_band_search/app/controllers/api/v1/bands_controller.rb
@@ -7,7 +7,7 @@ module Api
       before_action :set_cors_headers
 
        def set_cors_headers
-          response.set_header('Access-Control-Allow-Origin', 'http://localhost:4000')
+          response.set_header('Access-Control-Allow-Origin', 'https://react-rails-1.onrender.com')
           response.set_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
           response.set_header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With')
        end
