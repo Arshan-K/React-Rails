de752e132025c20118b92449f89d5f7665b85ce6
Arshan <arshankhan9326863763@gmail.com>
2024-11-27 12:54:45 +0530

    cors

--- music_band_search/config/initializers/cors.rb
+++ music_band_search/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4000'
+    origins 'https://react-rails-1.onrender.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
